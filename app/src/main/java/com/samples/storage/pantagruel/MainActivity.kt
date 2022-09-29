ba3528adbf0ec3af49773ce4c0d60b9631a6fddb
Yacine Rezgui <yrezgui@google.com>
2022-09-29 12:20:36 +0900

    Add files generated by Android Studio

---
+++ app/src/main/java/com/samples/storage/pantagruel/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.samples.storage.pantagruel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.samples.storage.pantagruel.ui.theme.PantagruelTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PantagruelTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PantagruelTheme {
+        Greeting("Android")
+    }
+}
